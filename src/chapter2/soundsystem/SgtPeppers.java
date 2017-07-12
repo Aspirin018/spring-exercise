0f127a2bbaa993eeef94fb9f2409e5c3d4557b13
liyu <liyu@PN-datang-ly.local>
2017-07-12 11:11:23 +0800

    naming component

--- src/chapter2/soundsystem/SgtPeppers.java
+++ src/chapter2/soundsystem/SgtPeppers.java
@@ -2,10 +2,14 @@ package chapter2.soundsystem;
 
 import org.springframework.stereotype.Component;
 
+import javax.inject.Named;
+
 /**
  * Created by liyu on 2017/7/12.
  */
 @Component
+//@Component("lonelyHeartsClub")
+//@Named("lonelyHeartsClub")//需要引入javax.inject包
 public class SgtPeppers implements CompactDisc {
     private String title = "Sgt.Pepper's lonely hearts club band";
     private String artist = "the beatles";
