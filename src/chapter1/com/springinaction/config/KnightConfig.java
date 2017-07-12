78bb55a1cf0aeade7fb117e55bb84b76dd108893
liyu <liyu@PN-datang-ly.local>
2017-07-12 09:47:21 +0800

    without aspect

--- src/chapter1/com/springinaction/config/KnightConfig.java
+++ src/chapter1/com/springinaction/config/KnightConfig.java
@@ -2,6 +2,7 @@ package chapter1.com.springinaction.config;
 
 import chapter1.com.springinaction.knights.BraveKnight;
 import chapter1.com.springinaction.knights.Knight;
+import chapter1.com.springinaction.minstrel.Minstrel;
 import chapter1.com.springinaction.quests.Quest;
 import chapter1.com.springinaction.quests.SlayDragonQuest;
 import org.springframework.context.annotation.Bean;
@@ -14,12 +15,14 @@ import org.springframework.context.annotation.Configuration;
 public class KnightConfig {
     @Bean
     public Knight knight(){
-        return new BraveKnight(quest());
+        return new BraveKnight(quest(), minstrel());
     }
 
     @Bean
     public Quest quest(){
         return new SlayDragonQuest(System.out);
     }
+
+    public Minstrel minstrel(){return new Minstrel(System.out);}
 
 }
--- src/chapter1/com/springinaction/tests/BraveKnightTest.java
+++ src/chapter1/com/springinaction/tests/BraveKnightTest.java
@@ -3,6 +3,7 @@ package chapter1.com.springinaction.tests;
 import static org.mockito.Mockito.*;
 
 import chapter1.com.springinaction.knights.BraveKnight;
+import chapter1.com.springinaction.minstrel.Minstrel;
 import chapter1.com.springinaction.quests.Quest;
 import org.junit.Test;
 
@@ -13,7 +14,8 @@ public class BraveKnightTest {
     @Test
     public void knightShouldEmbarkOnQust(){
         Quest mockQuest = mock(Quest.class);
-        BraveKnight knight = new BraveKnight(mockQuest);
+        Minstrel mockMinstrel = mock(Minstrel.class);
+        BraveKnight knight = new BraveKnight(mockQuest, mockMinstrel);
         knight.embarkOnQuest();
         verify(mockQuest, times(1)).embark();
     }
