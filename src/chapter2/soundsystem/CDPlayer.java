188d3ecf374197923487b652afad426870ba1d5f
liyu <liyu@PN-datang-ly.local>
2017-07-12 11:42:42 +0800

    autowire and require is false

--- src/chapter2/soundsystem/CDPlayer.java
+++ src/chapter2/soundsystem/CDPlayer.java
@@ -9,18 +9,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class CDPlayer implements MediaPlayer {
     private CompactDisc cd;
-//    @Autowired
-//    public CDPlayer(CompactDisc cd){
-//        this.cd = cd;
-//    }
+    @Autowired(required = false)
+    public CDPlayer(CompactDisc cd){
+        this.cd = cd;
+    }
 //    @Autowired
 //    public void setCompactDisc(CompactDisc cd){
 //        this.cd = cd;
 //    }
-    @Autowired
-    public void insertDisc(CompactDisc cd){
-        this.cd = cd;
-    }
+//    @Autowired
+//    public void insertDisc(CompactDisc cd){
+//        this.cd = cd;
+//    }
     @Override
     public void play() {
         cd.play();
