44e09c580fb0105c637f348663ea4eee2e26d5d0
liyu <liyu@PN-datang-ly.local>
2017-07-13 16:31:09 +0800

    use porfile annotation for class

---
+++ src/chapter3/assemble/DevelopmentProfileConfig.java
@@ -0,0 +1,24 @@
+package chapter3.assemble;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by liyu on 2017/7/13.
+ */
+@Configuration
+@Profile("dev")
+public class DevelopmentProfileConfig {
+    @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:test-data.sql")
+                .build();
+    }
+}
---
+++ src/chapter3/assemble/ProductionProfileConfig.java
@@ -0,0 +1,24 @@
+package chapter3.assemble;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by liyu on 2017/7/13.
+ */
+@Configuration
+@Profile("prod")
+public class ProductionProfileConfig {
+    @Bean
+    public DataSource dataSource(){
+        JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+        jndiObjectFactoryBean.setJndiName("jdbc/myDS");
+        jndiObjectFactoryBean.setResourceRef(true);
+        jndiObjectFactoryBean.setProxyInterface(javax.sql.DataSource.class);
+        return (DataSource) jndiObjectFactoryBean.getObject();
+    }
+}
