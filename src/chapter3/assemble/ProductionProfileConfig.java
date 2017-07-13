bf63715638122cc2af17892bdd43954f998588c9
liyu <liyu@PN-datang-ly.local>
2017-07-13 16:36:12 +0800

    use profile annotation for methods

--- src/chapter3/assemble/ProductionProfileConfig.java
+++ src/chapter3/assemble/ProductionProfileConfig.java
@@ -13,12 +13,12 @@ import javax.sql.DataSource;
 @Configuration
 @Profile("prod")
 public class ProductionProfileConfig {
-    @Bean
-    public DataSource dataSource(){
-        JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
-        jndiObjectFactoryBean.setJndiName("jdbc/myDS");
-        jndiObjectFactoryBean.setResourceRef(true);
-        jndiObjectFactoryBean.setProxyInterface(javax.sql.DataSource.class);
-        return (DataSource) jndiObjectFactoryBean.getObject();
-    }
+//    @Bean
+//    public DataSource dataSource(){
+//        JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+//        jndiObjectFactoryBean.setJndiName("jdbc/myDS");
+//        jndiObjectFactoryBean.setResourceRef(true);
+//        jndiObjectFactoryBean.setProxyInterface(javax.sql.DataSource.class);
+//        return (DataSource) jndiObjectFactoryBean.getObject();
+//    }
 }
