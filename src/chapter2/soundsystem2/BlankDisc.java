d4e4b469131506f08fba32c873b0e035ab79e8bc
liyu <liyu@PN-datang-ly.local>
2017-07-13 10:23:28 +0800

    add list as constructor-arg

--- src/chapter2/soundsystem2/BlankDisc.java
+++ src/chapter2/soundsystem2/BlankDisc.java
@@ -1,28 +1,33 @@
 package chapter2.soundsystem2;
 
 
+import java.util.List;
+
 /**
  * Created by liyu on 2017/7/13.
  */
 public class BlankDisc implements CompactDisc {
     private String title;
-//    private String artist;
+    private String artist;
+    private List<String> tracks;
 
-//    public BlankDisc(String title, String artist){
-//        this.title = title;
-//        this.artist = artist;
-//    }
-    public BlankDisc(String title){
+    public BlankDisc(String title, String artist, List tracks){
         this.title = title;
+        this.artist = artist;
+        this.tracks = tracks;
     }
+//    public BlankDisc(String title){
+//        this.title = title;
+//    }
 
-    @Override
-    public void play() {
-        System.out.println("title:" + title);
-    }
 //    @Override
 //    public void play() {
-//        System.out.println("title:" + title + ", artist:" + artist);
+//        System.out.println("title:" + title);
 //    }
+    @Override
+    public void play() {
+        System.out.println("title:" + title + ", artist:" + artist);
+        tracks.forEach(System.out::println);
+    }
 
 }
