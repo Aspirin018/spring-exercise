229e95c77d4dc1ebe3e2e431c61d07b7ee6d005d
liyu <liyu@PN-datang-ly.local>
2017-07-14 12:03:06 +0800

    primary bean in @component

--- src/chapter3/assemble/lunch/Cake.java
+++ src/chapter3/assemble/lunch/Cake.java
@@ -1,10 +1,12 @@
 package chapter3.assemble.lunch;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by liyu on 2017/7/14.
  */
 @Component
+@Primary
 public class Cake implements Dessert {
 }
