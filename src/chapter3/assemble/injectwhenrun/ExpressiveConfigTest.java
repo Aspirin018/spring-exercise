22aa95811e475ccada5758fb446b799fb4799623
liyu <liyu@PN-datang-ly.local>
2017-07-20 09:32:34 +0800

    env api

--- src/chapter3/assemble/injectwhenrun/ExpressiveConfigTest.java
+++ src/chapter3/assemble/injectwhenrun/ExpressiveConfigTest.java
@@ -2,16 +2,18 @@ package chapter3.assemble.injectwhenrun;
 
 import chapter2.soundsystem2.CompactDisc;
 import chapter2.soundsystem2.BlankDisc;
+import javafx.beans.DefaultProperty;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static junit.framework.TestCase.assertSame;
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 /**
  * Created by liyu on 2017/7/19.
@@ -19,6 +21,7 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ExpressiveConfig.class)
 @PropertySource("classpath:/chapter3/assemble/injectwhenrun/app.properties")
+@ActiveProfiles("prod")
 public class ExpressiveConfigTest {
 //    @Autowired
 //    private BlankDisc blankDisc;
@@ -38,10 +41,25 @@ public class ExpressiveConfigTest {
 //        );
 //    }
 
-    @Test
-    public void getClassFromEnv() {
-        Class<CompactDisc> cdClass = env.getPropertyAsClass("disc.class", CompactDisc.class);
-        assertSame("should be same", cdClass, BlankDisc.class);
-    }
+//    @Test
+//    public void getClassFromEnv() {
+//        Class<CompactDisc> cdClass = env.getPropertyAsClass("disc.class", CompactDisc.class);
+//        assertSame("should be same", cdClass, BlankDisc.class);
+//    }
 
+    @Test
+    public void getActiveProfileFromEnv() {
+//        String[] activeprofiles = env.getActiveProfiles();
+//        System.out.println(activeprofiles[0]);
+//
+//        String[] defaultProfiles = env.getDefaultProfiles();
+//        System.out.println(defaultProfiles[0]);
+//        String[] expectProfiles = {"prod"};
+//        assertArrayEquals(expectProfiles, activeprofiles);
+//        assertArrayEquals(expectProfiles, defaultProfiles);
+
+        boolean acceptProfiles = env.acceptsProfiles("prod");
+        System.out.println(acceptProfiles);
+        assertEquals(acceptProfiles, true);
+    }
 }
