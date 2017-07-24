b50c376712bbd710e1b7a964c5b5660dd892f045
liyu <liyu@PN-datang-ly.local>
2017-07-24 16:21:33 +0800

    around information in xml

--- src/chapter4/concert2/Audience.java
+++ src/chapter4/concert2/Audience.java
@@ -1,19 +1,32 @@
 package chapter4.concert2;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 /**
  * Created by liyu on 2017/7/24.
  */
 public class Audience {
-    public void silenceCellPhones(){
+//    public void silenceCellPhones(){
+//        System.out.println("silence cell phones");
+//    }
+//    public void takeSeats(){
+//        System.out.println("taking seats");
+//    }
+//    public void applause(){
+//        System.out.println("clap clap clap");
+//    }
+//    public void demandRefund(){
+//        System.out.println("demanding a refund");
+//    }
+    public void watchperformance(ProceedingJoinPoint jp){
+        try {
         System.out.println("silence cell phones");
-    }
-    public void takeSeats(){
         System.out.println("taking seats");
-    }
-    public void applause(){
+        jp.proceed();
         System.out.println("clap clap clap");
-    }
-    public void demandRefund(){
-        System.out.println("demanding a refund");
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            System.out.println("demanding a refund");
+        }
     }
 }
