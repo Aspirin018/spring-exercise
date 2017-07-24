71160d4a6bec4cfd0508e6211ebd348d8d25766b
liyu <liyu@PN-datang-ly.local>
2017-07-24 16:12:55 +0800

    define aspect in xml

---
+++ src/chapter4/concert2/Audience.java
@@ -0,0 +1,19 @@
+package chapter4.concert2;
+
+/**
+ * Created by liyu on 2017/7/24.
+ */
+public class Audience {
+    public void silenceCellPhones(){
+        System.out.println("silence cell phones");
+    }
+    public void takeSeats(){
+        System.out.println("taking seats");
+    }
+    public void applause(){
+        System.out.println("clap clap clap");
+    }
+    public void demandRefund(){
+        System.out.println("demanding a refund");
+    }
+}
---
+++ src/chapter4/concert2/Performance.java
@@ -0,0 +1,8 @@
+package chapter4.concert2;
+
+/**
+ * Created by liyu on 2017/7/24.
+ */
+public interface Performance {
+    void perform();
+}
