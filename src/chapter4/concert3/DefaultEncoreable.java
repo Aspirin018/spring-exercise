3920221882c893609ad8f2915099b168b1907181
liyu <liyu@PN-datang-ly.local>
2017-07-24 16:42:08 +0800

    xml中 通过切面引入新的功能

---
+++ src/chapter4/concert3/DefaultEncoreable.java
@@ -0,0 +1,11 @@
+package chapter4.concert3;
+
+/**
+ * Created by liyu on 2017/7/24.
+ */
+public class DefaultEncoreable implements Encoreable {
+    @Override
+    public void performEncore() {
+
+    }
+}
---
+++ src/chapter4/concert3/Performance.java
@@ -0,0 +1,8 @@
+package chapter4.concert3;
+
+/**
+ * Created by liyu on 2017/7/24.
+ */
+public interface Performance {
+    public void perform();
+}
