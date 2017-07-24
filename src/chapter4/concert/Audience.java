f45c6c69c9a9e665bd14f35f80d9ccc83a3554e6
liyu <liyu@PN-datang-ly.local>
2017-07-24 13:41:15 +0800

    pointcut annotation

--- src/chapter4/concert/Audience.java
+++ src/chapter4/concert/Audience.java
@@ -1,29 +1,46 @@
 package chapter4.concert;
 
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 
 /**
  * Created by liyu on 2017/7/24.
  */
 @Aspect
 public class Audience {
-    @Before("execution(* chapter4.concert.Performance.perform(..))")
+//    @Before("execution(* chapter4.concert.Performance.perform(..))")
+//    public void silenceCellPhones(){
+//        System.out.println("Silence cell phones");
+//    }
+//    @Before("execution(* chapter4.concert.Performance.perform(..))")
+//    public void takeSeats(){
+//        System.out.println("Taking seats");
+//    }
+//    @AfterReturning("execution(* chapter4.concert.Performance.perform(..))")
+//    public void applause(){
+//        System.out.println("CLAP CLAP CLAP");
+//    }
+//    @AfterThrowing("execution(* chapter4.concert.Performance.perform(..))")
+//    public void demandRefund(){
+//        System.out.println("Demanding a refund");
+//    }
+
+    @Pointcut("execution(* chpter4.concert.Performance.perform(..))")
+    public void performance(){};
+
+    @Before("performance()")
     public void silenceCellPhones(){
-        System.out.println("Silence cell phones");
+        System.out.println("silence cell phones");
     }
-    @Before("execution(* chapter4.concert.Performance.perform(..))")
+    @Before("performance()")
     public void takeSeats(){
-        System.out.println("Taking seats");
+        System.out.println("taking seats");
     }
-    @AfterReturning("execution(* chapter4.concert.Performance.perform(..))")
+    @AfterReturning("performance()")
     public void applause(){
-        System.out.println("CLAP CLAP CLAP");
+        System.out.println("clap clap clap");
     }
-    @AfterThrowing("execution(* chapter4.concert.Performance.perform(..))")
+    @AfterThrowing("performance()")
     public void demandRefund(){
-        System.out.println("Demanding a refund");
+        System.out.println("demanding a refund");
     }
 }
